cc07978375e51780baa36cbe68242eea87ed5484
Alisher <zinullayev09@gmail.com>
2023-07-10 23:01:05 +0600

    Initial Commit

---
+++ StopWatchApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StopWatchApp
+//
+//  Created by Alisher Zinullayev on 10.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
